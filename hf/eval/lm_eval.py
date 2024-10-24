b083a869f532054dad132f22fe499b5d6341e3d8
Jingyu6 <liujingyu@student.ethz.ch>
2024-10-23 19:54:54 -0700

    Refactored code structure

--- hf/eval/lm_eval.py
+++
@@ -1,7 +0,0 @@
-from lm_eval.__main__ import cli_evaluate
-
-from models.llama.monkey_patch_llama import monkey_patch_llama
-
-if __name__ == "__main__":
-    monkey_patch_llama()
-    cli_evaluate()
--- hf/models/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# A bunch of env set variables for global control
-VERBOSITY = int(os.environ.get("VERBOSITY", -1))
-LOOK_AHEAD_CNT = int(os.environ.get("LOOK_AHEAD_CNT", 8))
-KEEP = float(os.environ.get("KEEP", -2))
-SAVE_STATS_DIR = os.environ.get("SAVE_STATS_DIR", None)
-ALGO = os.environ.get("ALGO", "grad")
