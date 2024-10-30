0f9795546a5f066ae6077791fcb63a93ff268017
Jingyu6 <liujingyu@student.ethz.ch>
2024-10-30 09:55:56 -0700

    Added helper script to summarize results

---
+++ eval/long_bench/summarize_results.py
@@ -0,0 +1,24 @@
+import json
+import os
+
+from pyparsing import C
+
+result_dir = "../../local/long_bench/pred_e"
+print_dataset_name = True
+for exp_name in os.listdir(result_dir):
+    result_file = os.path.join(result_dir, exp_name, "result.json")
+    if not os.path.exists(result_file):
+        continue
+    with open(result_file, 'r') as f:
+        result = json.load(f)
+
+    if print_dataset_name:
+        print(",,,".join([f"{dn}" for dn in result]))
+        print_dataset_name = False
+    row =f"{exp_name} \t"
+    for dataset in result:
+        scores = result[dataset]
+        scores = f"{scores['0-4k']}\t{scores['4-8k']}\t{scores['8k+']}\t"
+        row += scores
+
+    print(row)
