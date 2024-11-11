1fe21f7d06381d4e3eed7ea29b8339f5c12a7740
Jingyu6 <liujingyu@student.ethz.ch>
2024-11-11 13:35:15 -0800

    Get slop mapping functions

--- speculative_prefill/vllm_patch/worker/look_ahead_spec_worker.py
+++ speculative_prefill/vllm_patch/worker/look_ahead_spec_worker.py
@@ -1,7 +1,8 @@
 from copy import deepcopy
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 
 import torch
+from vllm.attention.backends.flash_attn import FlashAttentionMetadata
 from vllm.model_executor.layers.sampler import SamplerOutput
 from vllm.sequence import ExecuteModelRequest, SequenceGroupMetadata
 from vllm.worker.worker import Worker
@@ -25,9 +26,18 @@ class LookAheadSpecWorker(Worker):
         if not non_empty:
             return execute_model_req
 
+        prefill_slot_mapping = None
+        decode_slot_mapping = None
+
         model_outputs: List[SamplerOutput] = []
 
         for itr in range(look_ahead_cnt):
+            prefill_slot_mapping, decode_slot_mapping = self._update_slot_mapping(
+                request, 
+                prefill_slot_mapping=prefill_slot_mapping, 
+                decode_slot_mapping=decode_slot_mapping
+            )
+
             model_output: List[SamplerOutput] = super().execute_model(
                 execute_model_req=request)
             assert (len(model_output) == 1), \
@@ -41,15 +51,76 @@ class LookAheadSpecWorker(Worker):
             )
             model_outputs.append(model_output)
 
-        # get the attention score somehow
-        assert self.kv_cache is not None
-        # layer_cnt, (2, num_blocks, block_size, num_kv_heads, head_size)
-        kv_caches = self.kv_cache[execute_model_req.virtual_engine]
+        self._get_attention_scores(
+            prefill_slot_mapping=prefill_slot_mapping, 
+            decode_slot_mapping=decode_slot_mapping
+        )
         
-        num_hidden_layers = getattr(
-            self.model_runner.model_config.hf_text_config, "num_hidden_layers")
+        exit()
+
+        # # get the attention score somehow
+        # assert self.kv_cache is not None
+        # # layer_cnt, (2, num_blocks, block_size, num_kv_heads, head_size)
+        # kv_caches = self.kv_cache[execute_model_req.virtual_engine]
         
-        print(request)
+        # num_hidden_layers = getattr(
+        #     self.model_runner.model_config.hf_text_config, "num_hidden_layers")
+        
+
+    def _get_attention_scores(
+        self, 
+        prefill_slot_mapping: Optional[List[torch.Tensor]], 
+        decode_slot_mapping: Optional[List[torch.Tensor]]
+    ):
+        pass
+
+    def _get_tensors_from_slot_mapping(
+        slot_mapping: torch.Tensor, 
+        cache_pos: int
+    ):
+        """
+            cache_pos = 0 means keys
+            cache_pos = 1 means values
+        """
+        assert cache_pos in [0, 1]
+        pass
+
+    def _update_slot_mapping(
+        self, 
+        execute_model_req: ExecuteModelRequest, 
+        prefill_slot_mapping: Optional[List[torch.Tensor]], 
+        decode_slot_mapping: Optional[List[torch.Tensor]]
+    ) -> Tuple[Optional[List[torch.Tensor]], Optional[List[torch.Tensor]]]:
+        model_input, _,  _ = self.prepare_input(
+            execute_model_req
+        )
+
+        self.model_runner.attn_state.begin_forward(model_input)
+        assert model_input.attn_metadata is not None
+        prefill_metadata: FlashAttentionMetadata = model_input.attn_metadata.prefill_metadata
+        decode_metadata: FlashAttentionMetadata = model_input.attn_metadata.decode_metadata
+
+        # exclusive check
+        assert bool(prefill_metadata is not None) != bool(decode_metadata is not None)
+
+        if prefill_metadata is not None:
+            return list(torch.split(
+                prefill_metadata.slot_mapping, 
+                prefill_metadata.seq_lens_tensor.tolist())), decode_slot_mapping
+        else:
+            assert decode_metadata.decode_metadata == 1
+            assert decode_metadata.num_decode_tokens == len(decode_metadata.slot_mapping)
+
+            if decode_slot_mapping == None:
+                decode_slot_mapping = list(torch.split(decode_metadata.slot_mapping, 1))
+            else:
+                for sample_idx in range(len(decode_slot_mapping)):
+                    decode_slot_mapping[sample_idx] = torch.concat([
+                            decode_slot_mapping[sample_idx], 
+                            decode_metadata.slot_mapping[sample_idx: sample_idx + 1]
+                        ], dim=0)
+
+            return prefill_slot_mapping, decode_slot_mapping
 
     def _extract_prompt_execute_model_req(
         self, 
