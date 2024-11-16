93cbb8d475a25693ccb32abfa74db1f1dac99d72
Jingyu6 <liujingyu@student.ethz.ch>
2024-11-16 00:19:09 -0800

    Experiment scripts for 70B

---
+++ experiments/run_long_bench_70b.sh
@@ -0,0 +1,12 @@
+cd ./eval/long_bench
+
+for exp in "pp1" "p1" "pp1_full" "p1_full" "pp1_full_lah8" "p1_full_lah8"; do
+    SPEC_CONFIG_PATH=../../local/config_${exp}.yaml python pred_vllm.py \
+        --model "meta-llama/Meta-Llama-3.1-70B-Instruct" \
+        --spec-model "meta-llama/Meta-Llama-3.1-8B-Instruct" \
+        --spec-prefill \
+        --tensor-parallel-size 8 \
+        --exp spec_${exp}
+
+    python eval.py --exp spec_${exp}
+done
