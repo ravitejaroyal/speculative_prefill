67f64e0806d18f848cd971917518997953073097
Jingyu6 <liujingyu@student.ethz.ch>
2024-11-22 15:21:40 -0800

    Updated qps server script

--- eval/qps_server.sh
+++ eval/qps_server.sh
@@ -1,31 +1,39 @@
-API_KEY=${2:-local_server}
-PORT=${3:-8888}
+SIZE=${2:-"70B"}
+API_KEY=${3:-local_server}
+PORT=${4:-8888}
+
+SPEC_NAME=meta-llama/Meta-Llama-3.1-8B-Instruct
+MODEL_NAME=meta-llama/Meta-Llama-3.1-${SIZE}-Instruct
 
 echo "Using port ${PORT} and api key ${API_KEY}"
 
+fuser -n tcp ${PORT}
+
 if [ "$1" = "native" ]; then
     echo "Starting native vllm serving"
-    vllm serve \
-        'meta-llama/Meta-Llama-3.1-8B-Instruct' \
+    python -m speculative_prefill.scripts serve \
+        ${MODEL_NAME} \
         --dtype auto \
-        --max-model-len 32768 \
-        --gpu-memory-utilization 0.95 \
+        --max-model-len 65536 \
+        --gpu-memory-utilization 0.9 \
         --enable-chunked-prefill=False \
+        --tensor-parallel-size 8 \
         --enforce-eager \
-        --max-num-seqs 1 \
+        --max-num-seqs 128 \
         --api-key=$API_KEY \
         --port=$PORT
 
 elif [ "$1" = "spec_prefill" ]; then
     echo "Starting vllm serving with speculative prefill" 
-    python -m speculative_prefill.scripts serve \
-        'meta-llama/Meta-Llama-3.1-8B-Instruct' \
+    SPEC_CONFIG_PATH=./configs/config_p1_full_lah8.yaml ENABLE_SP=$SPEC_NAME python -m speculative_prefill.scripts serve \
+        ${MODEL_NAME} \
         --dtype auto \
-        --max-model-len 32768 \
-        --gpu-memory-utilization 0.95 \
+        --max-model-len 65536 \
+        --gpu-memory-utilization 0.9 \
         --enable-chunked-prefill=False \
+        --tensor-parallel-size 8 \
         --enforce-eager \
-        --max-num-seqs 1 \
+        --max-num-seqs 128 \
         --api-key=$API_KEY \
         --port=$PORT
 else
