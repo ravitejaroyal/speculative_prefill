c8e7af91b606832c0c92997406c321a346b1f5ba
Jingyu6 <liujingyu@student.ethz.ch>
2024-11-25 23:32:47 -0800

    Updated qps to the right value.

--- experiments/run_qps.sh
+++ experiments/run_qps.sh
@@ -1,6 +1,6 @@
-NUM_SAMPLES=64
 OUTPUT_DIR="./local/outputs/qps"
-
+NUM_SAMPLES=50
+TIMEOUT=10
 mkdir -p $OUTPUT_DIR
 
 SIZE=${1:-"70B"}
@@ -22,13 +22,13 @@ python3 eval/qps_client.py \
     --qps 0.2 \
     --category $CATEGORY \
     --max-tokens 1 \
-    --timeout 35 \
-    --num-samples 2
+    --timeout $TIMEOUT \
+    --num-samples 4
 
 echo "Start real profiling"
 echo "" > $OUTPUT_DIR/${SIZE}_${3}_ttft_${CATEGORY}.txt
 
-for qps in 0.2 0.4 0.8 1.6 3.2 6.4 12.8 25.6 51.2 102.4; do
+for qps in 0.1 0.5 1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0 5.5 6.0 6.5 7.0 7.5 8.0; do
     echo "Sleep for 5 seconds"
     sleep 5
     python3 eval/qps_client.py \
@@ -36,8 +36,9 @@ for qps in 0.2 0.4 0.8 1.6 3.2 6.4 12.8 25.6 51.2 102.4; do
         --qps $qps \
         --category $CATEGORY \
         --max-tokens 1 \
-        --timeout 35 \
+        --timeout $(( $TIMEOUT + 5 )) \
         --num-samples $NUM_SAMPLES >> $OUTPUT_DIR/${SIZE}_${3}_ttft_${CATEGORY}.txt
+    
     if cat $OUTPUT_DIR/${SIZE}_${3}_ttft_${CATEGORY}.txt | grep -q "Found timeout in queries"; then
         exit 1
     fi
