b1d0abe7dbd51000a1d3e27c920602d25ee88c39
Jingyu6 <liujingyu@student.ethz.ch>
2024-11-27 19:49:55 -0800

    Added high qps scripts

--- experiments/run_qps_mdqa_hqps.sh
+++ experiments/run_qps_mdqa_hqps.sh
@@ -1,10 +1,10 @@
 OUTPUT_DIR="./local/outputs/qps"
 TIMEOUT=20
-NUM_SAMPLES=32
+NUM_SAMPLES=64
 mkdir -p $OUTPUT_DIR
 
 SIZE=${1:-"70B"}
-CATEGORY=summarization
+CATEGORY=multi-doc-qa
 
 if [ $SIZE == "70B" ]; then
     MODEL_NAME=meta-llama/Meta-Llama-3.1-70B-Instruct
@@ -26,9 +26,9 @@ python3 eval/qps_client.py \
     --num-samples 4
 
 echo "Start real profiling"
-echo "" > $OUTPUT_DIR/${SIZE}_${2}_ttft_${CATEGORY}.txt
+echo "" > $OUTPUT_DIR/${SIZE}_${2}_ttft_${CATEGORY}_hqps.txt
 
-for qps in 0.2 0.4 0.6 0.8 1.0 1.2 1.4 1.6 1.8 2.0 2.2 2.4 2.6 2.8 3.0; do
+for qps in 3.2 3.4 3.6 3.8 4.0 4.2 4.4 4.6 4.8 5.0 5.2 5.4 5.6 5.8 6.0 6.2 6.4 6.6 6.8 7.0 7.2 7.4 7.6 7.8 8.0; do
     echo "Sleep for 10 seconds"
     sleep 10
     python3 eval/qps_client.py \
@@ -36,9 +36,9 @@ for qps in 0.2 0.4 0.6 0.8 1.0 1.2 1.4 1.6 1.8 2.0 2.2 2.4 2.6 2.8 3.0; do
         --qps $qps \
         --category $CATEGORY \
         --timeout $(( $TIMEOUT + 5 )) \
-        --num-samples $NUM_SAMPLES >> $OUTPUT_DIR/${SIZE}_${2}_ttft_${CATEGORY}.txt
+        --num-samples $NUM_SAMPLES >> $OUTPUT_DIR/${SIZE}_${2}_ttft_${CATEGORY}_hqps.txt
 
-    if cat $OUTPUT_DIR/${SIZE}_${2}_ttft_${CATEGORY}.txt | grep -q "Found timeout in queries"; then
+    if cat $OUTPUT_DIR/${SIZE}_${2}_ttft_${CATEGORY}_hqps.txt | grep -q "Found timeout in queries"; then
         exit 1
     fi
 done
